de7bdfe7a2758fc158ff3fefedb3ba8a06b718ef
Arthur John Philipps Epiz <artianz225@gmail.com>
2023-10-09 11:09:57 +0800

    updating cart pricing section

--- src/helpers/calculateTotalAddtoCart.js
+++ src/helpers/calculateTotalAddtoCart.js
@@ -1,13 +1,41 @@
-const myTotal = document.getElementById('total');
+export default function updateTotalPrice() {
+  let totalPrice = 0;
 
-let sum = 0
+  const addedCartProductMainContainers = document.querySelectorAll('.added-cart-product-main-container');
 
-export default function calculateTotal() {
-  const totalPrices = document.querySelectorAll('.total-main-price');
-  sum = 0;
-  totalPrices.forEach(productPrice => {
-    sum += parseFloat(productPrice.textContent);
+  addedCartProductMainContainers.forEach((container) => {
+    const addeditemCartCheck = container.querySelector('.checked-products');
+
+    if (addeditemCartCheck.checked) {
+      const addedpTotalPrice = container.querySelector('.total-main-price');
+      const price = parseFloat(addedpTotalPrice.getAttribute('data-price'));
+      totalPrice += price;
+      container.style.backgroundColor = "rgba(192, 192, 192, 0.3)";
+      container.style.borderTop = "1px solid cadetblue";
+      container.style.borderBottom = "1px solid cadetblue";
+      container.style.borderRight = "1px solid cadetblue";
+    } else {
+      container.style.backgroundColor = "";
+      container.style.borderTop = "none";
+      container.style.borderBottom = "none";
+      container.style.borderRight = "none";
+    }
   });
-  // console.log(sum);
-  myTotal.textContent = sum;
+
+  document.getElementById('mytotal').textContent = totalPrice.toFixed(2);
+  console.log(totalPrice);
 }
+
+// const myTotal = document.getElementById('total');
+
+// let sum = 0
+
+// export default function calculateTotal() {
+//   const totalPrices = document.querySelectorAll('.total-main-price');
+//   sum = 0;
+//   totalPrices.forEach(productPrice => {
+//     sum += parseFloat(productPrice.textContent);
+//   });
+//   // console.log(sum);
+//   myTotal.textContent = sum;
+// }
--- src/helpers/displayApiData.js
+++ src/helpers/displayApiData.js
@@ -1,3 +1,5 @@
+import updateTotalPrice from "./calculateTotalAddtoCart";
+
 export function renderShopItems(shopItems) {
   const shoppingList = document.getElementById("shopping-list");
 
@@ -176,34 +178,6 @@ export function renderShopItems(shopItems) {
         itemContainer.appendChild(addedCartProductMainContainer);
         addeditemCartCheck.querySelector('.checked-products');
         addeditemCartCheck.addEventListener('change', updateTotalPrice);
-
-        function updateTotalPrice() {
-          let totalPrice = 0;
-        
-          const addedCartProductMainContainers = document.querySelectorAll('.added-cart-product-main-container');
-        
-          addedCartProductMainContainers.forEach((container) => {
-            const addeditemCartCheck = container.querySelector('.checked-products');
-        
-            if (addeditemCartCheck.checked) {
-              const addedpTotalPrice = container.querySelector('.total-main-price');
-              const price = parseFloat(addedpTotalPrice.getAttribute('data-price'));
-              totalPrice += price;
-              container.style.backgroundColor = "rgba(192, 192, 192, 0.3)";
-              container.style.borderTop = "1px solid cadetblue";
-              container.style.borderBottom = "1px solid cadetblue";
-              container.style.borderRight = "1px solid cadetblue";
-            } else {
-              container.style.backgroundColor = "";
-              container.style.borderTop = "none";
-              container.style.borderBottom = "none";
-              container.style.borderRight = "none";
-            }
-          });
-        
-          document.getElementById('mytotal').textContent = totalPrice.toFixed(2);
-          console.log(totalPrice);
-        }
 
         cartDeleteBtn.addEventListener('click', deleteCart);
 
